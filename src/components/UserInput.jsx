5fd10b1418a4b465f4c0f26f387f4f4f37149141
bruceathy <67832593+bruceathy@users.noreply.github.com>
2024-12-31 23:56:19 -0500

    update

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,14 +1,27 @@
 export default function UserInput() {
   return (
     <div id="user-input">
-      <label htmlFor="initial-investment">Initial Investment</label>
-      <input type="number" id="initial-investment" />
-      <label htmlFor="annual-investment">Annual Investment</label>
-      <input type="number" id="annual-investment" />
-      <label htmlFor="expected-return">Expected Return</label>
-      <input type="number" id="expected-return" />
-      <label htmlFor="duration">Duration</label>
-      <input type="number" id="duration" />
+      <div className="input-group">
+        <p>
+          <label htmlFor="initial-investment">Initial Investment</label>
+          <input type="number" required />
+        </p>
+        <p>
+          <label htmlFor="annual-investment">Annual Investment</label>
+          <input type="number" required />
+        </p>
+      </div>
+
+      <div className="input-group">
+        <p>
+          <label htmlFor="expected-return">Expected Return</label>
+          <input type="number" required />
+        </p>
+        <p>
+          <label htmlFor="duration">Duration</label>
+          <input type="number" required />
+        </p>
+      </div>
     </div>
   );
 }
