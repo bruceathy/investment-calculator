fe3dd448966cf6de2c95e390068282f3ab33a4d5
bruceathy <67832593+bruceathy@users.noreply.github.com>
2025-01-01 22:26:33 -0500

    finished app

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,25 +1,45 @@
-export default function UserInput() {
+export default function UserInput({ onChange, userInput }) {
   return (
     <div id="user-input">
       <div className="input-group">
         <p>
           <label htmlFor="initial-investment">Initial Investment</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            value={userInput.initialInvestment}
+            onChange={(e) => onChange("initialInvestment", e.target.value)}
+          />
         </p>
         <p>
           <label htmlFor="annual-investment">Annual Investment</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            value={userInput.annualInvestment}
+            onChange={(e) => onChange("annualInvestment", e.target.value)}
+          />
         </p>
       </div>
 
       <div className="input-group">
         <p>
           <label htmlFor="expected-return">Expected Return</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            value={userInput.expectedReturn}
+            onChange={(e) => onChange("expectedReturn", e.target.value)}
+          />
         </p>
         <p>
           <label htmlFor="duration">Duration</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            value={userInput.duration}
+            onChange={(e) => onChange("duration", e.target.value)}
+          />
         </p>
       </div>
     </div>
